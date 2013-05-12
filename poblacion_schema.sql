cb820946ba5552ad43bd3224eb76eb8031631984
elena <mariaelena.vazquez@gmail.com>
2013-05-12 13:25:16 -0400

    nuevo esquema para población

---
+++ poblacion_schema.sql
@@ -0,0 +1,3 @@
+INSERT INTO tipo_relacion (codigo, nombre, descripcion) VALUES
+('Padre Hijo','Relacion de padre a hijo','Relacion que se crea cuando los items son de la misma fase'),
+('Antecesor Sucesor','Relacion de antecesor y sucesor','Relacion que se crea cuando los items son de diferentes fase');
